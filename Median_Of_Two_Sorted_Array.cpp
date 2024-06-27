292f5ac831088bbd7ccc9bbabe9a1139b672d699
shubham99298 <83624288+shubham99298@users.noreply.github.com>
2024-06-27 10:54:38 +0530

    Initial commit

---
+++ Median_Of_Two_Sorted_Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+         vector<int>ans;
+
+         for(int i=0; i<nums1.size(); i++)
+         {
+            ans.push_back(nums1[i]);
+         }
+
+         for(int i=0; i<nums2.size(); i++){
+            ans.push_back(nums2[i]);
+         }
+
+         sort(ans.begin(), ans.end());
+
+         int n=ans.size(); //4
+
+         //Main logic here 
+         // odd and even size 
+         return n%2? ans[n/2]:(ans[n/2-1]+ans[n/2])/2.0;
+        
+         }
+};
